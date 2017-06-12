d77b9b8bdc98dcc6dbba169d547d9cfe075ee82d
cedric.walter <cedric.walter@gmail.com>
2017-06-12 13:12:37 +0200

    added ballot.sol

--- ballot.sol
+++ ballot.sol
@@ -1,78 +1,159 @@
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.11;
 
 
-contract Ballot {
+contract chairperson {
+    address public chairperson;
+
+    function chairperson() {
+        chairperson = msg.sender;
+    }
+
+    modifier onlyChairPerson {
+        if (msg.sender != chairperson) throw;
+
+        // Placeholder for function code with modifier onlyOwner
+        _;
+    }
+
+    function transferOwnership(address newChairPerson) onlyOwner {
+        chairperson = newChairPerson;
+    }
+}
+
+
+/// @title Voting with delegation, create ballot and let you choose one of `proposalNames`.
+contract Ballot is chairperson {
 
     struct Voter {
-    uint weight;
-    bool voted;
-    uint8 vote;
-    address delegate;
+    uint weight; // weight is accumulated by delegation
+    bool voted;  // if true, that person already voted
+    address delegate; // person delegated to
+    uint vote;   // index of the voted proposal
     }
 
     struct Proposal {
-    uint voteCount;
+    bytes32 name;   // short name (up to 32 bytes)
+    uint voteCount; // number of accumulated votes
     }
 
-    address chairperson;
+    mapping (address => Voter) public voters;
 
-    mapping (address => Voter) voters;
+    Proposal[] public proposals;
 
-    Proposal[] proposals;
-
-    /// Create a new ballot with $(_numProposals) different proposals.
-    function Ballot(uint8 _numProposals) {
+    function Ballot(bytes32[] proposalNames) {
         chairperson = msg.sender;
         voters[chairperson].weight = 1;
-        proposals.length = _numProposals;
+
+        // TODO improvement many transactions are needed to assign the rights to vote to all participants.
+        for (uint i = 0; i < proposalNames.length; i++) {
+            // `Proposal({...})` creates a temporary
+            // Proposal object and `proposals.push(...)`
+            // appends it to the end of `proposals`.
+            proposals.push(Proposal({
+            name : proposalNames[i],
+            voteCount : 0
+            }));
+        }
     }
 
-    /// Give $(voter) the right to vote on this ballot.
-    /// May only be called by $(chairperson).
-    function giveRightToVote(address voter) {
-        if (msg.sender != chairperson || voters[voter].voted)
-        return;
+    // Give `voter` the right to vote on this ballot.
+    // May only be called by `chairperson`.
+    function giveRightToVote(address voter) onlyChairPerson {
+        // If the argument of `require` evaluates to `false`,
+        // it terminates and reverts all changes to
+        // the state and to Ether balances. It is often
+        // a good idea to use this if functions are
+        // called incorrectly. But watch out, this
+        // will currently also consume all provided gas
+        // (this is planned to change in the future).
+        require(!voters[voter].voted);
         voters[voter].weight = 1;
     }
 
-    /// Delegate your vote to the voter $(to).
+    /// Delegate your vote to the voter `to`.
     function delegate(address to) {
-        Voter sender = voters[msg.sender];
         // assigns reference
-        if (sender.voted)
-        return;
+        Voter sender = voters[msg.sender];
+        require(!sender.voted);
 
-        while (voters[to].delegate != address(0) && voters[to].delegate != msg.sender)
-        to = voters[to].delegate;
+        // Self-delegation is not allowed.
+        require(to != msg.sender);
 
-        if (to == msg.sender)
-        return;
+        // Forward the delegation as long as
+        // `to` also delegated.
+        // In general, such loops are very dangerous,
+        // because if they run too long, they might
+        // need more gas than is available in a block.
+        // In this case, the delegation will not be executed,
+        // but in other situations, such loops might
+        // cause a contract to get "stuck" completely.
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
 
+            // We found a loop in the delegation, not allowed.
+            require(to != msg.sender);
+        }
+
+        // Since `sender` is a reference, this
+        // modifies `voters[msg.sender].voted`
         sender.voted = true;
         sender.delegate = to;
 
         Voter delegate = voters[to];
-        if (delegate.voted)
-        proposals[delegate.vote].voteCount += sender.weight;
-        else
-        delegate.weight += sender.weight;
-    }
-
-    /// Give a single vote to proposal $(proposal).
-    function vote(uint8 proposal) {
-        Voter sender = voters[msg.sender];
-        if (sender.voted || proposal >= proposals.length) return;
-        sender.voted = true;
-        sender.vote = proposal;
-        proposals[proposal].voteCount += sender.weight;
-    }
-
-    function winningProposal() constant returns (uint8 winningProposal) {
-        uint256 winningVoteCount = 0;
-        for (uint8 proposal = 0; proposal < proposals.length; proposal++)
-        if (proposals[proposal].voteCount > winningVoteCount) {
-            winningVoteCount = proposals[proposal].voteCount;
-            winningProposal = proposal;
+        if (delegate.voted) {
+            // If the delegate already voted,
+            // directly add to the number of votes
+            proposals[delegate.vote].voteCount += sender.weight;
+        }
+        else {
+            // If the delegate did not vote yet,
+            // add to her weight.
+            delegate.weight += sender.weight;
         }
     }
+
+    /// Give your vote (including votes delegated to you)
+    /// to proposal `proposals[proposal].name`.
+    function vote(uint proposal) {
+        Voter sender = voters[msg.sender];
+        require(!sender.voted);
+
+        sender.voted = true;
+        sender.vote = proposal;
+
+        // If `proposal` is out of the range of the array,
+        // this will throw automatically and revert all
+        // changes.
+        proposals[proposal].voteCount += sender.weight;
+
+        HasVoted(msg.sender);
+    }
+
+    /// @dev Computes the winning proposal taking all
+    /// previous votes into account.
+    function winningProposal() constant
+    returns (uint winningProposal)
+    {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            //TODO tight vote is not supported first proposal win if number of vote is identical
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal = p;
+
+            }
+        }
+    }
+
+    // Calls winningProposal() function to get the index
+    // of the winner contained in the proposals array and then
+    // returns the name of the winner
+    function winnerName() constant
+    returns (bytes32 winnerName)
+    {
+        winnerName = proposals[winningProposal()].name;
+    }
+
+    event HasVoted(address target);
+
 }
